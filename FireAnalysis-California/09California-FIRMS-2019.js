6ecece034dbe593fc0f389687dd4d5cd7d70c9ab
chromebook <chromebook@madicetea.me>
2019-06-05 10:16:55 +0000

    Updated FireAnalysis-California/09California-FIRMS-2019.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/09California-FIRMS-2019.js
+++ FireAnalysis-California/09California-FIRMS-2019.js
@@ -443,7 +443,7 @@ single = single.addBands(mask);
 //FIRMS False-Color Image Export
 Export.image.toDrive({
   image: single,
-  description: "FIRMS_2019average_ButteCounty_BigSquare",
+  description: "FIRMS_2019average_Paradise_BigSquare",
   folder: "California-Paradise_CampFire2018",
   region:Big_Square,
   scale:30.0,
