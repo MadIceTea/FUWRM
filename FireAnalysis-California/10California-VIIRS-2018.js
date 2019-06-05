2f97257a69794e6f1466b217c32eff1ec22202ee
chromebook <chromebook@madicetea.me>
2019-06-05 09:13:47 +0000

    Updated FireAnalysis-California/10California-VIIRS-2018.js (added 3 lines, deleted 3 lines).

--- FireAnalysis-California/10California-VIIRS-2018.js
+++ FireAnalysis-California/10California-VIIRS-2018.js
@@ -400,12 +400,12 @@ var viirs = collection.median(); //lighting composite, taking median values
 Map.setCenter(-121.619, 39.894, 10);
 
 //Display Layers on the Map with limited range of values.
-//Brightest value in Town of Paradise is max.
+//Brightest value in Town of Paradise during Camp Fire (~10) is max.
 //Minimum is set to 1 to eliminate street lighting.
 Map.addLayer(Paradise, {color: "acc235"}, "Town of Paradise", 1, 1);
-Map.addLayer(viirs,{bands:["avg_rad", "avg_rad", "cf_cvg"],min:1,max:15}, "yearly median nightmap", 0, 1);
+Map.addLayer(viirs,{bands:["avg_rad", "avg_rad", "cf_cvg"],min:1,max:10}, "median nightmap", 0, 1);
 var single = viirs.select("avg_rad");
-Map.addLayer(single,{bands:["avg_rad"],min:1,max:15},"average masked nightmap", 1, 0.9);
+Map.addLayer(single,{bands:["avg_rad"],min:1,max:10},"average masked nightmap", 1, 0.9);
 
 //debug
 print(collection);
