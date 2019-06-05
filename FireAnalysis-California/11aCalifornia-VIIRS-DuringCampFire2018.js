892339a9168643a451698371ed758598b9672fa5
chromebook <chromebook@madicetea.me>
2019-06-05 09:05:29 +0000

    Updated FireAnalysis-California/11aCalifornia-VIIRS-DuringCampFire2018.js (added 54 lines).

--- FireAnalysis-California/11aCalifornia-VIIRS-DuringCampFire2018.js
+++ FireAnalysis-California/11aCalifornia-VIIRS-DuringCampFire2018.js
@@ -388,3 +388,57 @@ var Paradise = /* color: #d63000 */ee.Geometry.Polygon(
           [-121.812967, 39.884304],
           [-121.873712, 39.883994]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+//Import images for November 2018 - the month of the Camp Fire.
+//VIIRS Lvl.1 product does not filter out light from fires -- so we should see a region in full flame.
+var collection = ee.ImageCollection("NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG")
+  .filterDate("2018-12-01","2019-01-01") // for Nov. 2018 (Camp Fire Time Period)
+  .filterBounds(Paradise); //around the Town of Paradise, California, USA
+
+var viirs = collection.median(); //lighting composite, taking median values
+
+//Center Map
+Map.setCenter(-121.619, 39.894, 10);
+
+//Display Layers on the Map with limited range of values.
+//Brightest value in Town of Paradise is max.
+//Minimum is set to 1 to eliminate street lighting.
+Map.addLayer(Paradise, {color: "acc235"}, "Town of Paradise", 1, 1);
+Map.addLayer(viirs,{bands:["avg_rad", "avg_rad", "cf_cvg"],min:1,max:42}, "median nightmap", 0, 1);
+var single = viirs.select("avg_rad");
+Map.addLayer(single,{bands:["avg_rad"],min:1,max:42},"average masked nightmap", 1, 0.9);
+
+//debug
+print(collection);
+print(viirs);
+print(single);
+
+//Export Process
+var vis = {
+  min: 1, 
+  max: 42,
+  gamma: 1.5,
+};
+
+// visualize image using visOpts above
+// turning it into 8-bit visible image.
+single = single.visualize(vis);
+
+// obtain native scale of avg_rad band
+var scale = single.projection().nominalScale().getInfo();
+
+// add an alpha channel as 4th band to mask no data regions
+var mask = single.mask().reduce(ee.Reducer.min())
+    .multiply(255).toByte();
+single = single.addBands(mask);
+
+//VIIRS True-Color Image Export
+Export.image.toDrive({
+  image: single,
+  description: "VIIRS_duringFire_Paradise",
+  folder: "California-Paradise_CampFire2018",
+  region:Paradise,
+  scale:30.0,
+  fileFormat: "GeoTIFF",
+  crs: "EPSG:3857",
+  formatOptions: {cloudOptimized: true}
+});
