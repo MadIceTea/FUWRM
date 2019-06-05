a9e9baecff9870ae7849968fc3cb0c3319585655
chromebook <chromebook@madicetea.me>
2019-06-05 11:11:09 +0000

    Updated FireAnalysis-California/05DuringCampFire2018_Paradise_Sentinel.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/05DuringCampFire2018_Paradise_Sentinel.js
+++ FireAnalysis-California/05DuringCampFire2018_Paradise_Sentinel.js
@@ -408,7 +408,7 @@ Map.setCenter(-121.619, 39.894, 10);
 //filtering Against Paradise at 1-year resolution
 var sentinel_AR = ee.ImageCollection("COPERNICUS/S2") //load Sentinel2 raws for the duration of the fire
 	.filterBounds(Paradise)
-	.filterDate("2018-11-08","2018-11-12")
+	.filterDate("2018-11-08","2018-11-25")
 	// No need to filter for cloudy scenes: smoke is a given in fires.
 	.select(SENTINEL_2_BANDS, STD_NAMES);
 
