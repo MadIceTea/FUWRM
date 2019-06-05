3f8479a3e4b14fde11bc1fcf24140d5f509e055a
chromebook <chromebook@madicetea.me>
2019-06-05 09:03:30 +0000

    Updated FireAnalysis-California/12California-VIIRS-2019.js (added 2 lines, deleted 2 lines).

--- FireAnalysis-California/12California-VIIRS-2019.js
+++ FireAnalysis-California/12California-VIIRS-2019.js
@@ -403,9 +403,9 @@ Map.setCenter(-121.619, 39.894, 10);
 //Brightest value in Town of Paradise is max.
 //Minimum is set to 1 to eliminate street lighting.
 Map.addLayer(Paradise, {color: "acc235"}, "Town of Paradise", 1, 1);
-Map.addLayer(viirs,{bands:["avg_rad", "avg_rad", "cf_cvg"],min:1,max:15}, "yearly median nightmap", 0, 1);
+Map.addLayer(viirs,{bands:["avg_rad", "avg_rad", "cf_cvg"],min:1,max:16.5}, "yearly median nightmap", 0, 1);
 var single = viirs.select("avg_rad");
-Map.addLayer(single,{bands:["avg_rad"],min:1,max:15},"average masked nightmap", 1, 0.9);
+Map.addLayer(single,{bands:["avg_rad"],min:1,max:16.5},"average masked nightmap", 1, 0.9);
 
 //debug
 print(collection);
