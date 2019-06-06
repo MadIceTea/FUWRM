c1da2775ed3c26994f4c1ee1be3fdba858f82d9c
chromebook <chromebook@madicetea.me>
2019-06-06 12:06:39 +0000

    Updated FireAnalysis-California/03DuringCampFire2018_Paradise_landsat8.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/03DuringCampFire2018_Paradise_landsat8.js
+++ FireAnalysis-California/03DuringCampFire2018_Paradise_landsat8.js
@@ -463,7 +463,7 @@ single = single.addBands(mask);
 //Landsat True-Color Image Export
 Export.image.toDrive({
   image: single,
-  description: "landsat8_duringEarlyFire_Paradise_BigSquare",
+  description: "landsat8_duringFire_Paradise_BigSquare",
   folder: "California-Paradise_CampFire2018",
   region:Big_Square,
   scale:30.0,
