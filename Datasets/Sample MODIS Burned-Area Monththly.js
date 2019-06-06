6a884b7d8dcccb83522018466f1637fd06aa8a0a
chromebook <chromebook@madicetea.me>
2019-06-06 14:33:00 +0000

    Updated Datasets/Sample MODIS Burned-Area Monththly.js (added 3 lines, deleted 3 lines).

--- Datasets/Sample MODIS Burned-Area Monththly.js
+++ Datasets/Sample MODIS Burned-Area Monththly.js
@@ -2,9 +2,9 @@ var dataset = ee.ImageCollection("MODIS/006/MCD64A1")
                   .filter(ee.Filter.date("2018-11-01", "2019-12-01"));
 var burnedArea = dataset.select("BurnDate");
 var burnedAreaVis = {
-  min: 0.0,
-  max: 1.0,
-  palette:["black","purple","white"]
+  min: 312.0, // Nov. 8th, 2018 (CampFire sparked)
+  max: 329.0, // Nov. 25th, 2018 (CampFire contained)
+  palette:["black","white","purple"]
 };
 Map.setCenter(-121.619, 39.894, 10);
 Map.addLayer(burnedArea, burnedAreaVis, "Burned Area");
