683405bbdd0ff5d2661f49346b9059a8f4ded89c
chromebook <chromebook@madicetea.me>
2019-06-06 13:42:05 +0000

    Created 15California-KBDI2018-preCampFire.js.

---
+++ 15California-KBDI2018-preCampFire.js
@@ -0,0 +1,15 @@
+//Center Map
+Map.setCenter(-121.619, 39.894, 10);
+
+var collection = ee.ImageCollection('UTOKYO/WTLAB/KBDI/v1')
+  .select("KBDI")
+  .filterDate("2018-01-01", "2018-11-07");
+var band_viz = {
+  min: 0,
+  max: 800,
+  palette: [
+    "Navy", "SkyBlue", "HoneyDew", "Green", "YellowGreen", "Peru", "Sienna",
+    "Maroon"
+  ]
+};
+Map.addLayer(collection.mean(), band_viz, "KBDI");
