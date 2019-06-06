3ac663ec6afe1eb5a0009de890e4ae36f7a52aac
chromebook <chromebook@madicetea.me>
2019-06-06 21:59:33 +0000

    Updated FireAnalysis-California/00dPostCampFire2018_Barren+Burn_landcoverSpectraChart.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/00dPostCampFire2018_Barren+Burn_landcoverSpectraChart.js
+++ FireAnalysis-California/00dPostCampFire2018_Barren+Burn_landcoverSpectraChart.js
@@ -412,7 +412,7 @@ Map.addLayer(points);
 
 // Define customization options.
 var options = {
-  title: "Landsat 8 SR spectra for polygons in the Town of Paradise, Post-Fire",
+  title: "Landsat 8 SR spectra comparison for barren land vs burned area in the Town of Paradise, Post-Fire",
   hAxis: {title: "Wavelength (micrometers)"},
   vAxis: {title: "Reflectance"},
   lineWidth: 1,
