46cddb18abfb89411079851ed4e4d506be19ee18
chromebook <chromebook@madicetea.me>
2019-06-06 14:23:21 +0000

    Updated Datasets/Sample MODIS Burned-Area Monththly.js (added 4 lines, deleted 4 lines).

--- Datasets/Sample MODIS Burned-Area Monththly.js
+++ Datasets/Sample MODIS Burned-Area Monththly.js
@@ -1,11 +1,11 @@
-var dataset = ee.ImageCollection('MODIS/051/MCD45A1')
-                  .filter(ee.Filter.date('2017-01-01', '2017-05-31'));
-var burnedArea = dataset.select('burndate');
+var dataset = ee.ImageCollection("MODIS/006/MCD64A1")
+                  .filter(ee.Filter.date("2017-01-01", "2017-05-31"));
+var burnedArea = dataset.select("burndate");
 var burnedAreaVis = {
   min: 0.0,
   max: 1.0,
 };
 Map.setCenter(6.746, 46.529, 2);
-Map.addLayer(burnedArea, burnedAreaVis, 'Burned Area');
+Map.addLayer(burnedArea, burnedAreaVis, "Burned Area");
 
 print(dataset);
