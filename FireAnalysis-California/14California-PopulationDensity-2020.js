ccaf5487a40de4bbab3ed4f287eb0b8ba4a67eea
chromebook <chromebook@madicetea.me>
2019-06-06 12:08:11 +0000

    Updated FireAnalysis-California/14California-PopulationDensity-2020.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/14California-PopulationDensity-2020.js
+++ FireAnalysis-California/14California-PopulationDensity-2020.js
@@ -394,7 +394,7 @@ var Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
           [-121.873712, 39.883994]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var population = ee.ImageCollection("CIESIN/GPWv4/population-count")
-  .filterDate("2018-12-31","2020-01-10") // for Dec. 2018 (Camp Fire Immediate Aftermath)
+  .filterDate("2018-12-31","2020-01-10") // for 2020 Global Map [Prediction]
   .filterBounds(Paradise); //around the Town of Paradise, California, USA
   
 //debug
