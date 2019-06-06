40eb2beddc99f908a0e849d28007e6d2cf53d88d
chromebook <chromebook@madicetea.me>
2019-06-06 08:13:01 +0000

    Updated FireAnalysis-California/14California-PopulationCount-2020.js (added 13 lines, deleted 1 line).

---
+++ FireAnalysis-California/14California-PopulationCount-2020.js
@@ -0,0 +1,13 @@
+var population = ee.ImageCollection("CIESIN/GPWv4/population-count")
+  .filterDate("2014-12-31","2019-01-01") // for Dec. 2018 (Camp Fire Immediate Aftermath)
+  .filterBounds(Paradise); //around the Town of Paradise, California, USA
+  
+//debug
+print(population);
+
+//Center Map
+Map.setCenter(-121.619, 39.894, 10);
+
+//Display Layers on the Map with limited range of values.
+Map.addLayer(Paradise, {color: "acc235"}, "Town of Paradise", 1, 1);
+Map.addLayer(population, {"bands":["population-count"],min:0,max:800,palette: ["purple", "blue", "red"]}, "population", 1, 0.9);
