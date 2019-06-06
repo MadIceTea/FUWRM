0414428a0a3c34c72b8b0aa8ce2db1dfd4c1e847
chromebook <chromebook@madicetea.me>
2019-06-06 13:47:06 +0000

    Updated Datasets/Sample KBDI.js (added 6 lines, deleted 6 lines).

--- Datasets/Sample KBDI.js
+++ Datasets/Sample KBDI.js
@@ -1,13 +1,13 @@
+//Center Map
+Map.setCenter(-179, 60, 2);
+
+//Load KBDI collection
 var collection = ee.ImageCollection("UTOKYO/WTLAB/KBDI/v1")
   .select("KBDI")
   .filterDate("2019-01-01", "2019-01-10");
 var band_viz = {
   min: 0,
   max: 800,
-  palette: [
-    "Navy", "SkyBlue", "HoneyDew", "Green", "YellowGreen", "Peru", "Sienna",
-    "Maroon"
-  ]
+  palette: ["Navy", "SkyBlue", "Green", "YellowGreen", "Yellow", "Orange", "DarkOrange", "Red"]
 };
 Map.addLayer(collection.mean(), band_viz, "KBDI");
-Map.setCenter(-179, 60, 2);
