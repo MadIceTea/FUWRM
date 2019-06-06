a41e2611d34a4fabccfef36ce5fe67571e95c240
chromebook <chromebook@madicetea.me>
2019-06-06 14:24:52 +0000

    Updated Datasets/Sample MODIS Burned-Area Monththly.js (added 1 line, deleted 1 line).

--- Datasets/Sample MODIS Burned-Area Monththly.js
+++ Datasets/Sample MODIS Burned-Area Monththly.js
@@ -5,7 +5,7 @@ var burnedAreaVis = {
   min: 0.0,
   max: 1.0,
 };
-Map.setCenter(6.746, 46.529, 2);
+Map.setCenter(-121.619, 39.894, 10);
 Map.addLayer(burnedArea, burnedAreaVis, "Burned Area");
 
 print(dataset);
