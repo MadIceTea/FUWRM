3a5d0b51af51ad176f315318b06d3eb717b2e827
chromebook <chromebook@madicetea.me>
2019-06-06 09:37:44 +0000

    Updated FireAnalysis-California/00aPreCampFire2018_LandCoverSampling_Chart.js (added 217 lines, deleted 3 lines).

--- FireAnalysis-California/00aPreCampFire2018_LandCoverSampling_Chart.js
+++ FireAnalysis-California/00aPreCampFire2018_LandCoverSampling_Chart.js
@@ -1,6 +1,142 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var Aquatic_Park = /* color: #00ffff */ee.Geometry.Point([-121.61385, 39.74934]),
-    Bille_Park = /* color: #ffc82d */ee.Geometry.Point([-121.6333, 39.77503]),
+var Community_Park = /* color: #bf04c2 */ee.Geometry.Polygon(
+        [[[-121.62440152502467, 39.75246215570849],
+          [-121.62330718374659, 39.754375777218804],
+          [-121.62255616522242, 39.754128329154184],
+          [-121.6236505065005, 39.75216520967495],
+          [-121.62440152502467, 39.75246215570849]]]),
+    Bille_Park = 
+    /* color: #ffc82d */
+    /* displayProperties: [
+      {
+        "type": "marker"
+      },
+      {
+        "type": "rectangle"
+      },
+      {
+        "type": "rectangle"
+      },
+      {
+        "type": "rectangle"
+      },
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry({
+      "type": "GeometryCollection",
+      "geometries": [
+        {
+          "type": "Point",
+          "coordinates": [
+            -121.6333,
+            39.77503
+          ]
+        },
+        {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                -121.63560051613365,
+                39.7773042987354
+              ],
+              [
+                -121.63560051613365,
+                39.7748470935795
+              ],
+              [
+                -121.63228530579124,
+                39.7748470935795
+              ],
+              [
+                -121.63228530579124,
+                39.7773042987354
+              ]
+            ]
+          ],
+          "geodesic": false,
+          "evenOdd": true
+        },
+        {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                -121.63556832962547,
+                39.77486358518166
+              ],
+              [
+                -121.63556832962547,
+                39.773222651402136
+              ],
+              [
+                -121.63322944336448,
+                39.773222651402136
+              ],
+              [
+                -121.63322944336448,
+                39.77486358518166
+              ]
+            ]
+          ],
+          "geodesic": false,
+          "evenOdd": true
+        },
+        {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                -121.63284320526634,
+                39.772958779002636
+              ],
+              [
+                -121.63284320526634,
+                39.770649852358616
+              ],
+              [
+                -121.63201708488975,
+                39.770649852358616
+              ],
+              [
+                -121.63201708488975,
+                39.772958779002636
+              ]
+            ]
+          ],
+          "geodesic": false,
+          "evenOdd": true
+        },
+        {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                -121.63308996849571,
+                39.77354424452139
+              ],
+              [
+                -121.63308996849571,
+                39.77333809525885
+              ],
+              [
+                -121.6318990676931,
+                39.77333809525885
+              ],
+              [
+                -121.6318990676931,
+                39.77354424452139
+              ]
+            ]
+          ],
+          "geodesic": false,
+          "evenOdd": true
+        }
+      ],
+      "coordinates": []
+    }),
     Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
         [[[-122.03757135752642, 40.231519880601745],
           [-122.03757135752642, 39.49365087730002],
@@ -393,7 +529,85 @@ var Aquatic_Park = /* color: #00ffff */ee.Geometry.Point([-121.61385, 39.74934])
           [-121.806104, 39.888217],
           [-121.804041, 39.884344],
           [-121.812967, 39.884304],
-          [-121.873712, 39.883994]]]);
+          [-121.873712, 39.883994]]]),
+    Aquatic_Memorial_Park = 
+    /* color: #07e8ff */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      },
+      {
+        "type": "marker"
+      },
+      {
+        "type": "marker"
+      },
+      {
+        "type": "marker"
+      },
+      {
+        "type": "marker"
+      }
+    ] */
+    ee.Geometry({
+      "type": "GeometryCollection",
+      "geometries": [
+        {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                -121.61444307919567,
+                39.751852734820844
+              ],
+              [
+                -121.61444307919567,
+                39.75012876501868
+              ],
+              [
+                -121.6136384164912,
+                39.75012876501868
+              ],
+              [
+                -121.6136384164912,
+                39.751852734820844
+              ]
+            ]
+          ],
+          "geodesic": false,
+          "evenOdd": true
+        },
+        {
+          "type": "Point",
+          "coordinates": [
+            -121.61346675511425,
+            39.75019475508752
+          ]
+        },
+        {
+          "type": "Point",
+          "coordinates": [
+            -121.61382080670421,
+            39.74990604806971
+          ]
+        },
+        {
+          "type": "Point",
+          "coordinates": [
+            -121.6145932829005,
+            39.74979881372638
+          ]
+        },
+        {
+          "type": "Point",
+          "coordinates": [
+            -121.61461474057262,
+            39.75053295319813
+          ]
+        }
+      ],
+      "coordinates": []
+    });
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 //Center Map
 Map.setCenter(-121.619, 39.894, 10);
