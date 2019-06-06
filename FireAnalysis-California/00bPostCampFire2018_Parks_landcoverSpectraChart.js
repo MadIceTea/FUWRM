32d04b27f2993b42d2c047126ddf105d7a912008
chromebook <chromebook@madicetea.me>
2019-06-06 09:59:18 +0000

    Updated FireAnalysis-California/00bPostCampFire2018_Parks_landcoverSpectraChart.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/00bPostCampFire2018_Parks_landcoverSpectraChart.js
+++ FireAnalysis-California/00bPostCampFire2018_Parks_landcoverSpectraChart.js
@@ -627,7 +627,7 @@ var Community_Park = /* color: #bf04c2 */ee.Feature(
 //Center Map
 Map.setCenter(-121.619, 39.894, 10);
 
-// We use a LANDSAT 8 image from  (~May 2018).
+// We use a LANDSAT 8 image from six months before the fire (June 1, 2018).
 var image = ee.Image("LANDSAT/LC08/C01/T1_SR/LC08_044032_20180601")
     .select(["B[1-7]"]);
 Map.addLayer(image, {bands: ["B4", "B3", "B2"], min: 0, max: 2000});
