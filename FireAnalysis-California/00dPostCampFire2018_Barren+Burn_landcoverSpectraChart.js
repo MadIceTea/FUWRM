7ef83e7be7205bfb25596115630815039ba3cdbd
chromebook <chromebook@madicetea.me>
2019-06-07 13:20:57 +0000

    Updated FireAnalysis-California/00dPostCampFire2018_Barren+Burn_landcoverSpectraChart.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/00dPostCampFire2018_Barren+Burn_landcoverSpectraChart.js
+++ FireAnalysis-California/00dPostCampFire2018_Barren+Burn_landcoverSpectraChart.js
@@ -560,7 +560,7 @@ Map.setCenter(-121.619, 39.894, 10);
 // We use a LANDSAT 8 image from about six months after the fire (May 3, 2019).
 var image = ee.Image("LANDSAT/LC08/C01/T1_SR/LC08_044032_20190503")
     .select(["B[1-7]"]);
-Map.addLayer(image, {bands: ["B4", "B3", "B2"], min: 0, max: 2000});
+Map.addLayer(image, {bands: ["B4", "B3", "B2"], min: 0, max: 2000}, "Landsat");
 
 // Define and display a FeatureCollection of three known locations.
 var points = ee.FeatureCollection([
