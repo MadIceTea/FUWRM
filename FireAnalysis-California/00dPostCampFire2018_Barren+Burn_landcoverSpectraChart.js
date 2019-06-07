099695cb35855e475a83d3278905f5a780de729f
chromebook <chromebook@madicetea.me>
2019-06-07 14:36:03 +0000

    Updated FireAnalysis-California/00dPostCampFire2018_Barren+Burn_landcoverSpectraChart.js (added 231 lines).

--- FireAnalysis-California/00dPostCampFire2018_Barren+Burn_landcoverSpectraChart.js
+++ FireAnalysis-California/00dPostCampFire2018_Barren+Burn_landcoverSpectraChart.js
@@ -197,6 +197,237 @@ var Barren1_CitySouth =
           "label": "BurntHouse4",
           "system:index": "0"
         }),
+    Community_Park = /* color: #bf04c2 */ee.Feature(
+        ee.Geometry.Polygon(
+            [[[-121.62440152502467, 39.75246215570849],
+              [-121.62330718374659, 39.754375777218804],
+              [-121.62255616522242, 39.754128329154184],
+              [-121.6236505065005, 39.75216520967495],
+              [-121.62440152502467, 39.75246215570849]]]),
+        {
+          "label": "Community Park",
+          "system:index": "0"
+        }),
+    Bille_Park = 
+    /* color: #ffc82d */
+    /* displayProperties: [
+      {
+        "type": "marker"
+      },
+      {
+        "type": "rectangle"
+      },
+      {
+        "type": "rectangle"
+      },
+      {
+        "type": "rectangle"
+      },
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Feature(
+        ee.Geometry({
+          "type": "GeometryCollection",
+          "geometries": [
+            {
+              "type": "Point",
+              "coordinates": [
+                -121.6333,
+                39.77503
+              ]
+            },
+            {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [
+                    -121.63560051613365,
+                    39.7773042987354
+                  ],
+                  [
+                    -121.63560051613365,
+                    39.7748470935795
+                  ],
+                  [
+                    -121.63228530579124,
+                    39.7748470935795
+                  ],
+                  [
+                    -121.63228530579124,
+                    39.7773042987354
+                  ]
+                ]
+              ],
+              "geodesic": false,
+              "evenOdd": true
+            },
+            {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [
+                    -121.63556832962547,
+                    39.77486358518166
+                  ],
+                  [
+                    -121.63556832962547,
+                    39.773222651402136
+                  ],
+                  [
+                    -121.63322944336448,
+                    39.773222651402136
+                  ],
+                  [
+                    -121.63322944336448,
+                    39.77486358518166
+                  ]
+                ]
+              ],
+              "geodesic": false,
+              "evenOdd": true
+            },
+            {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [
+                    -121.63284320526634,
+                    39.772958779002636
+                  ],
+                  [
+                    -121.63284320526634,
+                    39.770649852358616
+                  ],
+                  [
+                    -121.63201708488975,
+                    39.770649852358616
+                  ],
+                  [
+                    -121.63201708488975,
+                    39.772958779002636
+                  ]
+                ]
+              ],
+              "geodesic": false,
+              "evenOdd": true
+            },
+            {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [
+                    -121.63308996849571,
+                    39.77354424452139
+                  ],
+                  [
+                    -121.63308996849571,
+                    39.77333809525885
+                  ],
+                  [
+                    -121.6318990676931,
+                    39.77333809525885
+                  ],
+                  [
+                    -121.6318990676931,
+                    39.77354424452139
+                  ]
+                ]
+              ],
+              "geodesic": false,
+              "evenOdd": true
+            }
+          ],
+          "coordinates": []
+        }),
+        {
+          "label": "Bille Park",
+          "system:index": "0"
+        }),
+    Aquatic_Memorial_Park = 
+    /* color: #07e8ff */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      },
+      {
+        "type": "marker"
+      },
+      {
+        "type": "marker"
+      },
+      {
+        "type": "marker"
+      },
+      {
+        "type": "marker"
+      }
+    ] */
+    ee.Feature(
+        ee.Geometry({
+          "type": "GeometryCollection",
+          "geometries": [
+            {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [
+                    -121.61444307919567,
+                    39.751852734820844
+                  ],
+                  [
+                    -121.61444307919567,
+                    39.75012876501868
+                  ],
+                  [
+                    -121.6136384164912,
+                    39.75012876501868
+                  ],
+                  [
+                    -121.6136384164912,
+                    39.751852734820844
+                  ]
+                ]
+              ],
+              "geodesic": false,
+              "evenOdd": true
+            },
+            {
+              "type": "Point",
+              "coordinates": [
+                -121.61346675511425,
+                39.75019475508752
+              ]
+            },
+            {
+              "type": "Point",
+              "coordinates": [
+                -121.61382080670421,
+                39.74990604806971
+              ]
+            },
+            {
+              "type": "Point",
+              "coordinates": [
+                -121.6145932829005,
+                39.74979881372638
+              ]
+            },
+            {
+              "type": "Point",
+              "coordinates": [
+                -121.61461474057262,
+                39.75053295319813
+              ]
+            }
+          ],
+          "coordinates": []
+        }),
+        {
+          "label": "Aquatic (Memorial) Park",
+          "system:index": "0"
+        }),
     Paradise = /* color: #d63000 */ee.Geometry.Polygon(
         [[[-121.873712, 39.883994],
           [-121.856864, 39.840851],
