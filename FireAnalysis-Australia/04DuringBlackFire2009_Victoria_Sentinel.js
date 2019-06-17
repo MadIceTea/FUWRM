b901d70df19c37663ef0aea5e93eb5cfc8033cd7
chromebook <chromebook@madicetea.me>
2019-06-17 13:41:01 +0000

    Updated FireAnalysis-Australia/04DuringBlackFire2009_Victoria_Sentinel.js (added 14 lines, deleted 1 line).

---
+++ FireAnalysis-Australia/04DuringBlackFire2009_Victoria_Sentinel.js
@@ -0,0 +1,14 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var Big_Square = 
+    /* color: #ff6c64 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[144.84399398906248, -37.18091645226004],
+          [144.84399398906248, -38.13760786176182],
+          [146.16235336406248, -38.13760786176182],
+          [146.16235336406248, -37.18091645226004]]], null, false);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
