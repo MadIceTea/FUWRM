1cbedd64838a1821aa529151aff34ee961b7c318
chromebook <chromebook@madicetea.me>
2019-06-19 03:01:47 +0000

    Created FireAnalysis-California/06eLIDA_Paradise_MOLUSCE50kSim_2023.js.

---
+++ FireAnalysis-California/06eLIDA_Paradise_MOLUSCE50kSim_2023.js
@@ -0,0 +1,55 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var CARTclassified = ee.Image("users/GEE_Alex/LIDAParadise_2023_Simulation"),
+    Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
+        [[[-122.03757135752642, 40.231519880601745],
+          [-122.03757135752642, 39.49365087730002],
+          [-121.16965143565142, 39.49365087730002],
+          [-121.16965143565142, 40.231519880601745]]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+//Center Map
+Map.setCenter(-121.621, 39.762, 13);
+
+//Display the result using 0=barren, 1=urban, 2=green, 3=water
+Map.addLayer(CARTclassified, {min: 0, max: 3, palette: ["784800","FFF44F","228B22","97CAF9"]}, "CARTclassification", 1, 0.75);
+
+var single = CARTclassified;
+
+//Classification Image Export
+//Visualization Settings
+var vis = {
+  min: 0, 
+  max: 3,
+  palette: ["784800","FFF44F","228B22","97CAF9"]
+};
+
+// visualize image using visOpts above
+// turning it into 8-bit RGB image.
+single = single.visualize(vis);
+
+// obtain native scale of RGB bands
+var scale = single.projection().nominalScale().getInfo();
+
+// add an alpha channel as 4th band to mask no data regions
+var mask = single.mask().reduce(ee.Reducer.min())
+    .multiply(255).toByte();
+single = single.addBands(mask);
+
+//Landsat True-Color Image Export
+Export.image.toDrive({
+  image: single,
+  description: "classifiedImage_2023_MOLUSCEsim50k_Paradise_BigSquare",
+  folder: "California-Paradise_CampFire2018",
+  region:Big_Square,
+  scale:30.0,
+  fileFormat: "GeoTIFF",
+  crs: "EPSG:3857",
+  formatOptions: {cloudOptimized: true}
+});
+
+Export.image.toAsset({
+  image: single,
+  description: "classifiedImage_2023_MOLUSCEsim50k_Paradise_BigSquare-Asset",
+  region:Big_Square,
+  scale:30.0,
+  crs: "EPSG:3857"
+});
