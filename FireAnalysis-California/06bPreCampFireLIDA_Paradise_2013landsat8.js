da151ab8e0327f6cf01ce1cc54df6e11806d0e15
chromebook <chromebook@madicetea.me>
2019-06-19 15:47:03 +0000

    Updated FireAnalysis-California/06bPreCampFireLIDA_Paradise_2013landsat8.js (added 2 lines, deleted 2 lines).

--- FireAnalysis-California/06bPreCampFireLIDA_Paradise_2013landsat8.js
+++ FireAnalysis-California/06bPreCampFireLIDA_Paradise_2013landsat8.js
@@ -402,8 +402,8 @@ Map.setCenter(-121.621, 39.762, 13);
 
 //filtering Against Paradise at 1-year resolution
 
-var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 raws for during the fire period
-	.filterBounds(Paradise)
+var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 raws
+  .filterBounds(Paradise)
 	.filterDate("2013-01-01","2014-01-01") //note that there is no imagery for Jan-March 2013 with Landsat8
 	// Filter cloudy scenes.
   .filter(ee.Filter.lt("CLOUD_COVER", 35))
