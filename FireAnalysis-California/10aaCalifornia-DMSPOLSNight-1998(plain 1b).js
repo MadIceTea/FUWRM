0156cbf47e0a9b884ca9e71cb5712385668a2839
chromebook <chromebook@madicetea.me>
2019-06-19 07:19:16 +0000

    Updated FireAnalysis-California/10aaCalifornia-DMSPOLSNight-1998(plain 1b).js (added 1 line, deleted 1 line).

--- FireAnalysis-California/10aaCalifornia-DMSPOLSNight-1998(plain 1b).js
+++ FireAnalysis-California/10aaCalifornia-DMSPOLSNight-1998(plain 1b).js
@@ -13,7 +13,7 @@ var collection = ee.ImageCollection("NOAA/DMSP-OLS/NIGHTTIME_LIGHTS")
 var DMSP = collection.median(); //lighting composite, taking median values
 
 //Center Map
-Map.setCenter(-121.621, 39.762, 13);
+Map.setCenter(-121.619, 39.894, 10);
 
 //Display Layers on the Map with limited range of values.
 //Minimum is set to 1 to eliminate street lighting.
