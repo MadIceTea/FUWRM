42b2532db3eef1672a3aba77add8a806c448a23f
chromebook <chromebook@madicetea.me>
2019-06-23 18:21:40 +0000

    Created FireAnalysis-Australia/11LIDA_Victoria_2000.js.

---
+++ FireAnalysis-Australia/11LIDA_Victoria_2000.js
@@ -0,0 +1,124 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var Big_Square = 
+    /* color: #ff6c64 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[144.84399398906248, -37.18091645226004],
+          [144.84399398906248, -38.13760786176182],
+          [146.16235336406248, -38.13760786176182],
+          [146.16235336406248, -37.18091645226004]]], null, false);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// Import the Province of Victoria from Fusion Table
+var Victoria = ee.FeatureCollection("ft:1UzSGq1cWUA5PloR9VxO7AVTu4vVXL3BHNiJKv6XB").geometry();
+
+// Also import the metropolitan regions of Melbourne, Buxton-Narbethlong-Marysville, and the Kinglake Cities
+var Melbourne = ee.FeatureCollection("ft:1IS6OpUOtWinTQd2KKzFHIyLQEOy_Mf-Pg7f7j1qz").geometry();
+var BuxNarbMary = ee.FeatureCollection("ft:1hseXyjCm5NM3krhX5qJB6-Gkl0e5o84tBD1WnbeJ").geometry();
+var Kinglake = ee.FeatureCollection("ft:1uTaTWzmTW02jGVsMa6AggqK6ncqY2527kJQh1t1z").geometry();
+
+//Also import the small town regions of Labertouche, DrouinWest, and Tonimbuk which burned in the Bunyip State Park Fire
+var Labertouche = ee.FeatureCollection("ft:1Wsbt3y1em75OyN2vfvQhHjbI7XTptMFQFeT069n5").geometry();
+var DrouinWest = ee.FeatureCollection("ft:1g2cJ9z_o3gnFKfYxbPtF6DYgBOe-JUDoNHRhJqBQ").geometry();
+var Tonimbuk = ee.FeatureCollection("ft:1tqsM6Mt1HQD5QE_VF3vyDcj7beSlX0YQzHDSa2dd").geometry();
+
+// show the layers
+Map.addLayer(Victoria, {color: "55EAEC"}, "Province of Victoria, Australia", 1, 0.4); //light blue
+Map.addLayer(Melbourne, {color: "000000"}, "Melbourne, VIC, Australia", 1, 0.3); //black
+Map.addLayer(BuxNarbMary, {color: "BF19DB"}, "Buxton-Narbelthong-Marysville, VIC, Australia", 1, 1); //purple
+Map.addLayer(Kinglake, {color: "31994D"}, "Kinglake, VIC, Australia", 1, 1); //green
+Map.addLayer(Labertouche, {color: "270BFF"}, "Labertouche, VIC, Australia", 1, 1); //royal-deep blue
+Map.addLayer(DrouinWest, {color: "C18AB9"}, "Drouin West, VIC, Australia", 1, 1); //purple
+Map.addLayer(Tonimbuk, {color: "ADC91F"}, "Tonimbuk, VIC, Australia", 1, 1); //darker green
+
+//Center Map
+Map.centerObject(Big_Square, 9);
+
+//L7SR Bands and Human-Friendly Naming
+var LANDSAT_7_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
+var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
+
+//filtering Against the entire export region from December 2019 to April 2019 (sample representation)
+var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws for during the fire period
+	.filterBounds(Big_Square)
+	.filterDate("2000-01-01", "2001-01-01")
+	// Filter cloudy scenes.
+  .filter(ee.Filter.lt("CLOUD_COVER", 5))
+	.select(LANDSAT_7_BANDS, STD_NAMES);
+
+print(landsat_SR); //date debug
+
+var median = landsat_SR.median();
+
+//Display the Composite
+Map.addLayer(landsat_SR, {"bands":["red","green","blue"],min:0,max:2000}, "baselayer", 0, 1);
+
+function addNDVI(image) {
+  return image
+    .addBands(image.normalizedDifference(["nir","red"]).rename("ndvi"))
+  ;
+}
+
+var ndvi = addNDVI(median);
+
+Map.addLayer(ndvi,{bands:["ndvi"],min:0,max:1}, "ndvilayer", 0, 1);
+
+//predict bands
+var predictionBands = ["blue","green","red","nir","swir1","swir2","ndvi"];
+
+var trainingimage = ndvi.select(predictionBands);
+
+//fusion-table of polygons drawn in Google Earth Desktop
+var trainingpolygons = ee.FeatureCollection("ft:1YBmiKZM2pDv08IX6RIUMl_47Lw8UNE6FTqk1KA2L");
+
+var training = trainingimage.sampleRegions({
+    collection: trainingpolygons,
+    properties: ["class"],
+    scale: 30
+});
+
+//Train the CART classifier (a regular expression, not made up) with default parameters
+var trained = ee.Classifier.cart().train(training,"class", predictionBands);
+
+//Classify image with the same bands used for training.
+var CARTclassified = trainingimage.select(predictionBands).classify(trained);
+
+//Display the result using 0=barren, 1=urban, 2=green, 3=water
+Map.addLayer(CARTclassified, {min: 0, max: 3, palette: ["784800","FFF44F","228B22","97CAF9"]}, "CARTclassification", 1, 0.75);
+
+var single = CARTclassified;
+
+//Classification Image Export
+//Visualization Settings
+var vis = {
+  min: 0, 
+  max: 3,
+  palette: ["784800","FFF44F","228B22","97CAF9"]
+};
+
+// visualize image using visOpts above
+// turning it into 8-bit RGB image.
+single = single.visualize(vis);
+
+// obtain native scale of RGB bands
+var scale = single.projection().nominalScale().getInfo();
+
+// add an alpha channel as 4th band to mask no data regions
+var mask = single.mask().reduce(ee.Reducer.min())
+    .multiply(255).toByte();
+single = single.addBands(mask);
+
+//Landsat True-Color Image Export
+Export.image.toDrive({
+  image: single,
+  description: "classifiedImage_2000_lowCloudVictoria_BigSquare",
+  folder: "Australia-Victoria_BlackFire2009",
+  region:Big_Square,
+  scale:30.0,
+  fileFormat: "GeoTIFF",
+  crs: "EPSG:3857",
+  formatOptions: {cloudOptimized: true},
+});
