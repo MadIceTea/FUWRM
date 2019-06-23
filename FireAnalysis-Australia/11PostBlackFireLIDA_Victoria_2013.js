82cd4959c4c308620660650923717ce6c4b93f18
chromebook <chromebook@madicetea.me>
2019-06-23 19:21:27 +0000

    Updated FireAnalysis-Australia/11PostBlackFireLIDA_Victoria_2013.js (added 2 lines, deleted 2 lines).

--- FireAnalysis-Australia/11PostBlackFireLIDA_Victoria_2013.js
+++ FireAnalysis-Australia/11PostBlackFireLIDA_Victoria_2013.js
@@ -44,7 +44,7 @@ var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 //filtering Against the entire export region for one-year (2010).
 var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws
 	.filterBounds(Melbourne)
-	.filterDate("2010-01-01", "2011-01-01")
+	.filterDate("2013-01-01", "2014-01-01")
 	// Filter cloudy scenes.
   .filter(ee.Filter.lt("CLOUD_COVER", 5))
 	.select(LANDSAT_7_BANDS, STD_NAMES);
@@ -114,7 +114,7 @@ single = single.addBands(mask);
 //Landsat True-Color Image Export
 Export.image.toDrive({
   image: single,
-  description: "classifiedImage_postFire2010_Victoria_BigSquare",
+  description: "classifiedImage_postFire2013_Victoria_BigSquare",
   folder: "Australia-Victoria_BlackFire2009",
   region:Big_Square,
   scale:30.0,
