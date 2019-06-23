3a8d5608f3b36c404ac0b11a41638d20360b05c9
chromebook <chromebook@madicetea.me>
2019-06-23 18:28:45 +0000

    Updated FireAnalysis-Australia/12LIDA_Victoria_1998.js (added 3 lines, deleted 3 lines).

--- FireAnalysis-Australia/12LIDA_Victoria_1998.js
+++ FireAnalysis-Australia/12LIDA_Victoria_1998.js
@@ -38,16 +38,16 @@ Map.addLayer(Tonimbuk, {color: "ADC91F"}, "Tonimbuk, VIC, Australia", 1, 1); //d
 Map.centerObject(Big_Square, 9);
 
 //L7SR Bands and Human-Friendly Naming
-var LANDSAT_7_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
+var LANDSAT_5_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
 //filtering Against the entire export region from December 2019 to April 2019 (sample representation)
-var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws for during the fire period
+var landsat_SR = ee.ImageCollection("LANDSAT/LT05/C01/T1_SR") //load LANDSAT7 raws for during the fire period
 	.filterBounds(Big_Square)
 	.filterDate("1998-01-01", "1999-01-01")
 	// Filter cloudy scenes.
   .filter(ee.Filter.lt("CLOUD_COVER", 5))
-	.select(LANDSAT_7_BANDS, STD_NAMES);
+	.select(LANDSAT_5_BANDS, STD_NAMES);
 
 print(landsat_SR); //date debug
 
