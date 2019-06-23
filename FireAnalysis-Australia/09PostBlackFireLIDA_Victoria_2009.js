3c94f73e0ca38dc000abc62569cf88aecf870e26
chromebook <chromebook@madicetea.me>
2019-06-23 05:21:39 +0000

    Updated FireAnalysis-Australia/09PostBlackFireLIDA_Victoria_2009.js (added 1 line, deleted 1 line).

--- FireAnalysis-Australia/09PostBlackFireLIDA_Victoria_2009.js
+++ FireAnalysis-Australia/09PostBlackFireLIDA_Victoria_2009.js
@@ -72,7 +72,7 @@ var predictionBands = ["blue","green","red","nir","swir1","swir2","ndvi"];
 var trainingimage = ndvi.select(predictionBands);
 
 //fusion-table of polygons drawn in Google Earth Desktop
-var trainingpolygons = ee.FeatureCollection("ft:1fJTAvmqprnXrKiKjCfjevexnlQUeCV_XErp6zQIO");
+var trainingpolygons = ee.FeatureCollection("ft:1D53VTXOm3Gynh5VkbMQNjxjP6yGknqmFHKxngejY");
 
 var training = trainingimage.sampleRegions({
     collection: trainingpolygons,
