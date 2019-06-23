a2b198d08fbb5ba327b3e3dd317ba1b30b1ea440
chromebook <chromebook@madicetea.me>
2019-06-23 18:40:32 +0000

    Updated FireAnalysis-Australia/10aLIDA_Victoria_2019landsat7.js (added 1 line, deleted 1 line).

--- FireAnalysis-Australia/10aLIDA_Victoria_2019landsat7.js
+++ FireAnalysis-Australia/10aLIDA_Victoria_2019landsat7.js
@@ -42,7 +42,7 @@ var LANDSAT_7_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
 //filtering Against the entire export region from December 2019 to April 2019 (sample representation)
-var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws for during the fire period
+var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws
 	.filterBounds(Big_Square)
 	.filterDate("2018-12-01", "2019-05-01")
 	// Filter cloudy scenes.
