a555133f72916568e9a71e1e3813a9ae9c22b687
chromebook <chromebook@madicetea.me>
2019-06-23 02:26:14 +0000

    Updated FireAnalysis-Australia/08VictoriaLIDA-2009.js (added 34 lines, deleted 11 lines).

--- FireAnalysis-Australia/08VictoriaLIDA-2009.js
+++ FireAnalysis-Australia/08VictoriaLIDA-2009.js
@@ -1,17 +1,40 @@
-//bands to be considered
-var Landsat_8_BANDS = ["B2","B3","B4","B5","B6","B7"];
-var STD_NAMES = ["blue","green","red","nir","swir1","swir2"];
+// Import the Province of Victoria from Fusion Table
+var Victoria = ee.FeatureCollection("ft:1UzSGq1cWUA5PloR9VxO7AVTu4vVXL3BHNiJKv6XB").geometry();
+
+// Also import the metropolitan regions of Melbourne, Buxton-Narbethlong-Marysville, and the Kinglake Cities
+var Melbourne = ee.FeatureCollection("ft:1IS6OpUOtWinTQd2KKzFHIyLQEOy_Mf-Pg7f7j1qz").geometry();
+var BuxNarbMary = ee.FeatureCollection("ft:1hseXyjCm5NM3krhX5qJB6-Gkl0e5o84tBD1WnbeJ").geometry();
+var Kinglake = ee.FeatureCollection("ft:1uTaTWzmTW02jGVsMa6AggqK6ncqY2527kJQh1t1z").geometry();
+
+//Also import the small town regions of Labertouche, DrouinWest, and Tonimbuk which burned in the Bunyip State Park Fire
+var Labertouche = ee.FeatureCollection("ft:1Wsbt3y1em75OyN2vfvQhHjbI7XTptMFQFeT069n5").geometry();
+var DrouinWest = ee.FeatureCollection("ft:1g2cJ9z_o3gnFKfYxbPtF6DYgBOe-JUDoNHRhJqBQ").geometry();
+var Tonimbuk = ee.FeatureCollection("ft:1tqsM6Mt1HQD5QE_VF3vyDcj7beSlX0YQzHDSa2dd").geometry();
+
+// show the layers
+Map.addLayer(Victoria, {color: "55EAEC"}, "Province of Victoria, Australia", 1, 0.4); //light blue
+Map.addLayer(Melbourne, {color: "000000"}, "Melbourne, VIC, Australia", 1, 0.3); //black
+Map.addLayer(BuxNarbMary, {color: "BF19DB"}, "Buxton-Narbelthong-Marysville, VIC, Australia", 1, 1); //purple
+Map.addLayer(Kinglake, {color: "31994D"}, "Kinglake, VIC, Australia", 1, 1); //green
+Map.addLayer(Labertouche, {color: "270BFF"}, "Labertouche, VIC, Australia", 1, 1); //royal-deep blue
+Map.addLayer(DrouinWest, {color: "C18AB9"}, "Drouin West, VIC, Australia", 1, 1); //purple
+Map.addLayer(Tonimbuk, {color: "ADC91F"}, "Tonimbuk, VIC, Australia", 1, 1); //darker green
 
 //Center Map
-Map.setCenter(-121.621, 39.762, 13);
+Map.centerObject(Big_Square, 9);
 
-//filtering against Paradise at [about] half-year resolution
-var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load collection 1 - LANDSAT8 raws post-CampFire
-	.filterBounds(Big_Square)
-	.filterDate("2009-02-20","2019-06-01")
+//L7SR Bands and Human-Friendly Naming
+var LANDSAT_7_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
+var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
+
+//filtering Against Melbourne region at time resolution during the fire
+
+var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws for during the fire period
+	.filterBounds(Melbourne)
+	.filterDate("2009-02-16", "2010-01-01")
 	// Filter cloudy scenes.
   .filter(ee.Filter.lt("CLOUD_COVER", 35))
-	.select(Landsat_8_BANDS, STD_NAMES);
+	.select(Landsat_7_BANDS, STD_NAMES);
 
 print(landsat_SR); //date debug
 
