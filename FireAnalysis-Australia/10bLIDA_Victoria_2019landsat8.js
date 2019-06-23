7ab078a09102245e46c6a6d738fcab8f5b8bc3c9
chromebook <chromebook@madicetea.me>
2019-06-23 18:52:43 +0000

    Updated FireAnalysis-Australia/10bLIDA_Victoria_2019landsat8.js (added 1 line, deleted 1 line).

--- FireAnalysis-Australia/10bLIDA_Victoria_2019landsat8.js
+++ FireAnalysis-Australia/10bLIDA_Victoria_2019landsat8.js
@@ -46,7 +46,7 @@ var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 ra
 	.filterBounds(Big_Square)
 	.filterDate("2018-12-01", "2019-05-01")
 	// Filter cloudy scenes.
-  .filter(ee.Filter.lt("CLOUD_COVER", 5))
+  .filter(ee.Filter.lt("CLOUD_COVER", 10))
 	.select(LANDSAT_7_BANDS, STD_NAMES);
 
 print(landsat_SR); //date debug
