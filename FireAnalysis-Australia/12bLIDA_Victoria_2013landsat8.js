82f27c785550656fffa0a5b4937e3c6b6fe7340d
chromebook <chromebook@madicetea.me>
2019-06-23 21:25:35 +0000

    Updated FireAnalysis-Australia/12bLIDA_Victoria_2013landsat8.js (added 1 line, deleted 1 line).

--- FireAnalysis-Australia/12bLIDA_Victoria_2013landsat8.js
+++ FireAnalysis-Australia/12bLIDA_Victoria_2013landsat8.js
@@ -42,7 +42,7 @@ var LANDSAT_8_BANDS = ["B2", "B3", "B4","B5","B6","B10","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
 //filtering Against the entire export region for one-year (2013).
-var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT7 raws
+var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 raws
 	.filterBounds(Melbourne)
 	.filterDate("2013-04-11", "2014-01-01")
 	// Filter cloudy scenes.
