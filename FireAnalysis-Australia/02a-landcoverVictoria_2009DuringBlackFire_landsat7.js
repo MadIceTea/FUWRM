09024da65896cdb7a778c3a0617dd735be74c3a1
chromebook <chromebook@madicetea.me>
2019-06-24 07:09:39 +0000

    Updated FireAnalysis-Australia/02a-landcoverVictoria_2009DuringBlackFire_landsat7.js (added 1 line, deleted 1 line).

--- FireAnalysis-Australia/02a-landcoverVictoria_2009DuringBlackFire_landsat7.js
+++ FireAnalysis-Australia/02a-landcoverVictoria_2009DuringBlackFire_landsat7.js
@@ -98,7 +98,7 @@ single = single.addBands(mask);
 //Landsat True-Color Image Export
 Export.image.toDrive({
   image: single,
-  description: "landsat_duringFire2009_Victoria_BigSquare",
+  description: "landsat_duringFire2009l7_Victoria_BigSquare",
   folder: "Australia-Victoria_BlackFire2009",
   region:Big_Square,
   scale:30.0,
