2d914c62653983f14efa423e106e178a198a3f4b
chromebook <chromebook@madicetea.me>
2019-06-24 07:13:31 +0000

    Updated FireAnalysis-Australia/04b-landcoverVictoria_2018landsat8.js (added 2 lines, deleted 2 lines).

--- FireAnalysis-Australia/04b-landcoverVictoria_2018landsat8.js
+++ FireAnalysis-Australia/04b-landcoverVictoria_2018landsat8.js
@@ -45,10 +45,10 @@ var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
 var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 raws
 	.filterBounds(Melbourne)
-	.filterDate("2008-01-01","2009-01-01")
+	.filterDate("2018-01-01","2019-01-01")
 	// Filter cloudy scenes.
   .filter(ee.Filter.lt("CLOUD_COVER", 10))
-	.select(LANDSAT_8_BANDS, STD_NAMES);
+	.select(LANDSAT_7_BANDS, STD_NAMES);
 
 print(landsat_SR); //date debug
 
@@ -98,7 +98,7 @@ single = single.addBands(mask);
 //Landsat True-Color Image Export
 Export.image.toDrive({
   image: single,
-  description: "landsat_preFire2008_Victoria_BigSquare",
+  description: "landsat_2018l8_Victoria_BigSquare",
   folder: "Australia-Victoria_BlackFire2009",
   region:Big_Square,
   scale:30.0,
