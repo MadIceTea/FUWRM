3b4b7891f9c6dbe66770b9f1095018af5fade48f
chromebook <chromebook@madicetea.me>
2019-06-24 05:07:02 +0000

    Updated FireAnalysis-Australia/05b-landcoverVictoria_2013landsat8.js (added 1 line, deleted 1 line).

--- FireAnalysis-Australia/05b-landcoverVictoria_2013landsat8.js
+++ FireAnalysis-Australia/05b-landcoverVictoria_2013landsat8.js
@@ -43,7 +43,7 @@ var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
 //filtering Against Victoria at 1-year resolution
 
-var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws for during the fire period
+var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 raws for period available (mid-April to December 2013)
 	.filterBounds(Melbourne)
 	.filterDate("2008-01-01","2009-01-01")
 	// Filter cloudy scenes.
