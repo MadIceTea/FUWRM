810350ece2e8200f28b7e5d8daf2e24b665f15f5
chromebook <chromebook@madicetea.me>
2019-06-26 08:07:37 +0000

    Updated FireAnalysis-Australia/31LIDA_Victoria_MOLUSCE50kSim_2018.js (added 40 lines, deleted 11 lines).

--- FireAnalysis-Australia/31LIDA_Victoria_MOLUSCE50kSim_2018.js
+++ FireAnalysis-Australia/31LIDA_Victoria_MOLUSCE50kSim_2018.js
@@ -1,14 +1,43 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
-        [[[-122.03757135752642, 40.231519880601745],
-          [-122.03757135752642, 39.49365087730002],
-          [-121.16965143565142, 39.49365087730002],
-          [-121.16965143565142, 40.231519880601745]]]);
+var Big_Square = 
+    /* color: #ff6c64 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[144.84399398906248, -37.18091645226004],
+          [144.84399398906248, -38.13760786176182],
+          [146.16235336406248, -38.13760786176182],
+          [146.16235336406248, -37.18091645226004]]], null, false);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
-var CARTclassified = ee.Image("users/GEE_Alex/MOLUSCEsims_USA/LIDAParadise_2023_Simulation");
+var CARTclassified = ee.Image("users/GEE_Alex/MOLUSCEsims_AUS/LIDAVictoria_2018_Simulation");
+
+// Import the Province of Victoria from Fusion Table
+var Victoria = ee.FeatureCollection("ft:1UzSGq1cWUA5PloR9VxO7AVTu4vVXL3BHNiJKv6XB").geometry();
+
+// Also import the metropolitan regions of Melbourne, Buxton-Narbethlong-Marysville, and the Kinglake Cities
+var Melbourne = ee.FeatureCollection("ft:1IS6OpUOtWinTQd2KKzFHIyLQEOy_Mf-Pg7f7j1qz").geometry();
+var BuxNarbMary = ee.FeatureCollection("ft:1hseXyjCm5NM3krhX5qJB6-Gkl0e5o84tBD1WnbeJ").geometry();
+var Kinglake = ee.FeatureCollection("ft:1uTaTWzmTW02jGVsMa6AggqK6ncqY2527kJQh1t1z").geometry();
+
+//Also import the small town regions of Labertouche, DrouinWest, and Tonimbuk which burned in the Bunyip State Park Fire
+var Labertouche = ee.FeatureCollection("ft:1Wsbt3y1em75OyN2vfvQhHjbI7XTptMFQFeT069n5").geometry();
+var DrouinWest = ee.FeatureCollection("ft:1g2cJ9z_o3gnFKfYxbPtF6DYgBOe-JUDoNHRhJqBQ").geometry();
+var Tonimbuk = ee.FeatureCollection("ft:1tqsM6Mt1HQD5QE_VF3vyDcj7beSlX0YQzHDSa2dd").geometry();
+
+// show the layers
+Map.addLayer(Victoria, {color: "55EAEC"}, "Province of Victoria, Australia", 1, 0.4); //light blue
+Map.addLayer(Melbourne, {color: "000000"}, "Melbourne, VIC, Australia", 1, 0.3); //black
+Map.addLayer(BuxNarbMary, {color: "BF19DB"}, "Buxton-Narbelthong-Marysville, VIC, Australia", 1, 1); //purple
+Map.addLayer(Kinglake, {color: "31994D"}, "Kinglake, VIC, Australia", 1, 1); //green
+Map.addLayer(Labertouche, {color: "270BFF"}, "Labertouche, VIC, Australia", 1, 1); //royal-deep blue
+Map.addLayer(DrouinWest, {color: "C18AB9"}, "Drouin West, VIC, Australia", 1, 1); //purple
+Map.addLayer(Tonimbuk, {color: "ADC91F"}, "Tonimbuk, VIC, Australia", 1, 1); //darker green
 
 //Center Map
-Map.setCenter(-121.621, 39.762, 13);
+Map.centerObject(Big_Square, 9);
 
 //Display the result using 0=barren, 1=urban, 2=green, 3=water
 Map.addLayer(CARTclassified, {min: 0, max: 3, palette: ["784800","FFF44F","228B22","97CAF9"]}, "CARTclassification", 1, 0.75);
@@ -38,8 +67,8 @@ single = single.addBands(mask);
 //Landsat True-Color Image Export
 Export.image.toDrive({
   image: single,
-  description: "classifiedImage_2023_MOLUSCEsim50k_Paradise_BigSquare",
-  folder: "California-Paradise_CampFire2018",
+  description: "classifiedImage_2018_MOLUSCEsim50k_Paradise_BigSquare",
+  folder: "Australia-Victoria_BlackFire2009",
   region:Big_Square,
   scale:30.0,
   fileFormat: "GeoTIFF",
@@ -49,7 +78,7 @@ Export.image.toDrive({
 
 Export.image.toAsset({
   image: single,
-  description: "classifiedImage_2023_MOLUSCEsim50k_Paradise_BigSquare-Asset",
+  description: "classifiedImage_2018_MOLUSCEsim50k_Victoria_BigSquare-Asset",
   region:Big_Square,
   scale:30.0,
   crs: "EPSG:3857"
