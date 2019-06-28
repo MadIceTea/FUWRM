a9aefca70ccea80cc3b80a9aab613fb2b894e551
chromebook <chromebook@madicetea.me>
2019-06-28 01:50:41 +0000

    Created FireAnalysis-California/01-landcoverParadise_1998landsat5.js.

---
+++ FireAnalysis-California/01-landcoverParadise_1998landsat5.js
@@ -0,0 +1,98 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var Big_Square = 
+    /* color: #ff6c64 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[144.84399398906248, -37.18091645226004],
+          [144.84399398906248, -38.13760786176182],
+          [146.16235336406248, -38.13760786176182],
+          [146.16235336406248, -37.18091645226004]]], null, false);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// Import the primary region of interest, Town of Paradise which burned in the Camp Fire of 2018.
+var Paradise = ee.FeatureCollection("ft:1JIO1SLcMe08lHJWUIP7zWpW5razN6FfIwibHtcje").geometry();
+
+//Also import the small town region of Magalia to the north, and the larger city of Chico to the west.
+var Magalia = ee.FeatureCollection("ft:1BCMRnYS4plV2NVWtP6dZHYmE1V00kY8baAPU9Udm").geometry();
+var Chico = ee.FeatureCollection("ft:1mmRj4fN8mmvtynTxG56XMZJ-1y9n1i-lDUCIsXwV").geometry();
+
+// show the layers
+Map.addLayer(Paradise, {color: "55EAEC"}, "Town of Paradise, California", 1, 0.4); //light blue
+Map.addLayer(Magalia, {color: "ADC91F"}, "Town of Magalia, California", 1, 0.3); //darker green
+Map.addLayer(Chico, {color: "BF19DB"}, "City of Chico, California", 1, 1); //purple
+
+//Center Map
+Map.centerObject(Big_Square, 9);
+
+//L5SR Bands and Human-Friendly Naming
+var LANDSAT_5_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
+var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
+
+//filtering Against Victoria at 1-year resolution
+
+var landsat_SR = ee.ImageCollection("LANDSAT/LT05/C01/T1_SR") //load LANDSAT5 raws
+	.filterBounds(Paradise)
+	.filterDate("1998-01-01","1999-01-01")
+	// Filter cloudy scenes.
+  .filter(ee.Filter.lt("CLOUD_COVER", 35))
+	.select(LANDSAT_5_BANDS, STD_NAMES);
+
+print(landsat_SR); //date debug
+
+var single = landsat_SR.median();
+
+//Display the Composite
+Map.addLayer(landsat_SR, {"bands":["red","green","blue"],min:0,max:2000}, "baselayer", 1, 0);
+//Map.addLayer(landsat_SR, {"bands":["tir"],min:0,max:2000}, "temperature", 1, 1);
+
+var inputimage = landsat_SR.median();
+
+function addNDVI(image) {
+  return image
+    .addBands(image.normalizedDifference(["nir","red"]).rename("ndvi"))
+  ;
+}
+
+var ndvi = addNDVI(inputimage);
+
+//Toggle-display the single median-reduced image.
+Map.addLayer(single, {"bands":["red","green","blue"],min:0,max:2000}, "median_image", 1, 0.85);
+
+//Map of NDVI vegetation-water probability.
+Map.addLayer(ndvi,{bands:["ndvi"],min:0,max:1}, "ndvilayer", 1, 0.15);
+
+//Landsat True-Color Image Export
+//Export Image
+var vis = {
+  min: 100, 
+  max: 2000,
+  gamma: 1.5,
+  bands: ["red","green","blue"]
+};
+
+// visualize image using visOpts above
+// turning it into 8-bit RGB image.
+single = single.visualize(vis);
+
+// obtain native scale of RGB bands
+var scale = single.projection().nominalScale().getInfo();
+
+// add an alpha channel as 4th band to mask no data regions
+var mask = single.mask().reduce(ee.Reducer.min())
+    .multiply(255).toByte();
+single = single.addBands(mask);
+
+//Landsat True-Color Image Export
+Export.image.toDrive({
+  image: single,
+  description: "landsat_1998l5_Paradise_BigSquare",
+  folder: "California-Paradise_CampFire2018",
+  region:Big_Square,
+  scale:30.0,
+  fileFormat: "GeoTIFF",
+  crs: "EPSG:3857",
+  formatOptions: {cloudOptimized: true}
+});
