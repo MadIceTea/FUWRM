2307de00eacc1f6c530580475eff9606198c125c
chromebook <chromebook@madicetea.me>
2019-06-28 03:32:55 +0000

    Updated FireAnalysis-Australia/01-landcoverVictoria_1998landsat5.js (deleted 1 line).

--- FireAnalysis-Australia/01-landcoverVictoria_1998landsat5.js
+++ FireAnalysis-Australia/01-landcoverVictoria_1998landsat5.js
@@ -42,7 +42,6 @@ var LANDSAT_5_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
 //filtering Against Victoria at 1-year resolution
-
 var landsat_SR = ee.ImageCollection("LANDSAT/LT05/C01/T1_SR") //load LANDSAT5 raws
 	.filterBounds(Melbourne)
 	.filterDate("1998-01-01","1999-01-01")
