5245c933500ef5771512423faf41903a55b3ff56
chromebook <chromebook@madicetea.me>
2019-06-28 03:57:54 +0000

    Updated FireAnalysis-California/03-landcoverParadise_2008landsat7.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/03-landcoverParadise_2008landsat7.js
+++ FireAnalysis-California/03-landcoverParadise_2008landsat7.js
@@ -24,7 +24,7 @@ Map.centerObject(Paradise, 10);
 var LANDSAT_7_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
-//Filtering against Paradise at 1-year resolution...
+//Filtering against Paradise at 1-year resolution.
 var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws
 	.filterBounds(Paradise)
 	.filterDate("2008-01-01","2009-01-01")
