56e5a1391c687c221ca5dd17a6b0074fe37bf857
chromebook <chromebook@madicetea.me>
2019-06-28 05:48:39 +0000

    Updated FireAnalysis-California/20cLIDA_Paradise_2018PostCampFire_landsat7.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/20cLIDA_Paradise_2018PostCampFire_landsat7.js
+++ FireAnalysis-California/20cLIDA_Paradise_2018PostCampFire_landsat7.js
@@ -24,7 +24,7 @@ Map.centerObject(Paradise, 10);
 var LANDSAT_7_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
-//Filtering against Paradise at one-year resolution.
+//Filtering against Paradise at one-month resolution, for December 2018 -- immediately following the fire.
 var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws
 	.filterBounds(Paradise)
 	.filterDate("2018-11-26","2019-01-01")
