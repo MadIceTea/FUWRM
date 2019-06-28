01c45c1c2a3b3c9028178dbd299c1fe04d3aa507
chromebook <chromebook@madicetea.me>
2019-06-28 08:48:36 +0000

    Updated FireAnalysis-California/32California-DMSPOLSNight-2008(1b).js (added 19 lines, deleted 9 lines).

--- FireAnalysis-California/32California-DMSPOLSNight-2008(1b).js
+++ FireAnalysis-California/32California-DMSPOLSNight-2008(1b).js
@@ -393,7 +393,22 @@ var Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
           [-121.812967, 39.884304],
           [-121.873712, 39.883994]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
-//Import images for 2008, around a decade before the Camp Fire.
+// Import the primary region of interest, Town of Paradise which burned in the Camp Fire of 2018.
+var Paradise = ee.FeatureCollection("ft:1JIO1SLcMe08lHJWUIP7zWpW5razN6FfIwibHtcje").geometry();
+
+//Also import the small town region of Magalia to the north, and the larger city of Chico to the west.
+var Magalia = ee.FeatureCollection("ft:1BCMRnYS4plV2NVWtP6dZHYmE1V00kY8baAPU9Udm").geometry();
+var Chico = ee.FeatureCollection("ft:1mmRj4fN8mmvtynTxG56XMZJ-1y9n1i-lDUCIsXwV").geometry();
+
+// show the layers
+Map.addLayer(Paradise, {color: "280AC2"}, "Town of Paradise, California", 1, 1); //deep purple
+Map.addLayer(Magalia, {color: "91184E"}, "Town of Magalia, California", 1, 1); //reddish-purple
+Map.addLayer(Chico, {color: "1C06C2"}, "City of Chico, California", 1, 1); //deep blue
+
+//Center Map
+Map.centerObject(Paradise, 10);
+
+//Import images for 2008.
 //Use DMSP-OLS dataset Nighttime lights set.
 var collection = ee.ImageCollection("NOAA/DMSP-OLS/NIGHTTIME_LIGHTS")
   .filterDate("2008-01-01","2009-01-01") // for 2008
@@ -401,20 +416,15 @@ var collection = ee.ImageCollection("NOAA/DMSP-OLS/NIGHTTIME_LIGHTS")
   
 var DMSP = collection.median(); //lighting composite, taking median values
 
-//Center Map
-Map.setCenter(-121.619, 39.894, 10);
-
 //Display Layers on the Map with limited range of values.
-//Brightest value in Town of Paradise during Camp Fire (~10) is max.
-//Minimum is set to 1 to eliminate street lighting.
-Map.addLayer(DMSP,{bands:["avg_vis", "stable_lights", "cf_cvg"],min:0,max:60}, "median nightmap", 0, 1);
+Map.addLayer(DMSP,{bands:["avg_vis", "stable_lights", "cf_cvg"],min:0,max:63}, "median nightmap", 0, 1);
 var single = DMSP.select("stable_lights");
-Map.addLayer(single,{bands:["stable_lights"],min:0,max:60,palette: ["black", "orange", "white"]},"average cleaned nightmap", 1, 1);
+Map.addLayer(single,{bands:["stable_lights"],min:0,max:63,palette: ["black", "orange", "white"]},"average cleaned nightmap", 1, 0.85);
 
 //DMSP Image Export
 Export.image.toDrive({
   image: single,
-  description: "DMSPColored_2008_Paradise_BigSquare-1band",
+  description: "DMSPColored_2008_Paradise_BigSquare-1b",
   folder: "California-Paradise_CampFire2018",
   region:Big_Square,
   scale:30.0
