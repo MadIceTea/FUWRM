24a1770595145072798cd1f5f102ea49a44d80dc
chromebook <chromebook@madicetea.me>
2019-06-28 03:14:42 +0000

    Updated FireAnalysis-California/07a-landcoverParadise_2018DuringCampFire_landsat8.js (added 15 lines, deleted 6 lines).

--- FireAnalysis-California/07a-landcoverParadise_2018DuringCampFire_landsat8.js
+++ FireAnalysis-California/07a-landcoverParadise_2018DuringCampFire_landsat8.js
@@ -5,15 +5,24 @@ var Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
           [-121.16965143565142, 39.49365087730002],
           [-121.16965143565142, 40.231519880601745]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+// Import the primary region of interest, Town of Paradise which burned in the Camp Fire of 2018.
+var Paradise = ee.FeatureCollection("ft:1JIO1SLcMe08lHJWUIP7zWpW5razN6FfIwibHtcje").geometry();
+
+//Also import the small town region of Magalia to the north, and the larger city of Chico to the west.
+var Magalia = ee.FeatureCollection("ft:1BCMRnYS4plV2NVWtP6dZHYmE1V00kY8baAPU9Udm").geometry();
+var Chico = ee.FeatureCollection("ft:1mmRj4fN8mmvtynTxG56XMZJ-1y9n1i-lDUCIsXwV").geometry();
+
+// show the layers
+Map.addLayer(Paradise, {color: "280AC2"}, "Town of Paradise, California", 1, 1); //deep purple
+Map.addLayer(Magalia, {color: "91184E"}, "Town of Magalia, California", 1, 1); //reddish-purple
+Map.addLayer(Chico, {color: "1C06C2"}, "City of Chico, California", 1, 1); //deep blue
+
+//Center Map
+Map.centerObject(Paradise, 10);
+
 //L8SR Bands and Human-Friendly Naming
 var LANDSAT_8_BANDS = ["B2", "B3", "B4","B5","B6","B10","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
-
-//Add an outline of the Town of Paradise
-Map.addLayer(Paradise, {color: "000000"}, "Town of Paradise", 1, 1);
-
-//Center Map
-Map.setCenter(-121.619, 39.894, 10);
 
 //filtering Against Paradise at 1-year resolution
 var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 raws for during the fire period
