6a305e25cf0dada682567a19d7adff17ab1c2744
chromebook <chromebook@madicetea.me>
2019-06-28 03:47:34 +0000

    Updated FireAnalysis-Australia/02-landcoverVictoria_2000landsat7.js (added 1 line, deleted 1 line).

--- FireAnalysis-Australia/02-landcoverVictoria_2000landsat7.js
+++ FireAnalysis-Australia/02-landcoverVictoria_2000landsat7.js
@@ -41,7 +41,7 @@ Map.centerObject(Big_Square, 9);
 var LANDSAT_7_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
-//filtering Against Victoria at 1-year resolution
+//Filtering against the Melbourne region at 1-year resolution.
 var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws
 	.filterBounds(Melbourne)
 	.filterDate("2000-01-01","2001-01-01")
