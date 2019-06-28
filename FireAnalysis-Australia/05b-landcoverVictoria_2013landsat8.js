178ce53a81d416bd3a2748490d0c97039f7daea1
chromebook <chromebook@madicetea.me>
2019-06-28 03:56:23 +0000

    Updated FireAnalysis-Australia/05b-landcoverVictoria_2013landsat8.js (added 1 line, deleted 1 line).

--- FireAnalysis-Australia/05b-landcoverVictoria_2013landsat8.js
+++ FireAnalysis-Australia/05b-landcoverVictoria_2013landsat8.js
@@ -41,7 +41,7 @@ Map.centerObject(Big_Square, 9);
 var LANDSAT_8_BANDS = ["B2", "B3", "B4","B5","B6","B10","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
-//filtering Against the entire export region for one-year (2013).
+//filtering Against the entire export region for almost one-year (2013).
 //Jan - early April 2013 data is not available yet for 2013.
 var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 raws
 	.filterBounds(Melbourne)
