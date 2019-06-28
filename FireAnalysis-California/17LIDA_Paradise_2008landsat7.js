c7b6ec67264779e2f7984b7662fa5ef7a44db48d
chromebook <chromebook@madicetea.me>
2019-06-28 05:33:30 +0000

    Created FireAnalysis-California/17LIDA_Paradise_2008landsat7.js.

---
+++ FireAnalysis-California/17LIDA_Paradise_2008landsat7.js
@@ -0,0 +1,82 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
+        [[[-122.03757135752642, 40.231519880601745],
+          [-122.03757135752642, 39.49365087730002],
+          [-121.16965143565142, 39.49365087730002],
+          [-121.16965143565142, 40.231519880601745]]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// Import the primary region of interest, Town of Paradise which burned in the Camp Fire of 2018.
+var Paradise = ee.FeatureCollection("ft:1JIO1SLcMe08lHJWUIP7zWpW5razN6FfIwibHtcje").geometry();
+
+//Also import the small town region of Magalia to the north, and the larger city of Chico to the west.
+var Magalia = ee.FeatureCollection("ft:1BCMRnYS4plV2NVWtP6dZHYmE1V00kY8baAPU9Udm").geometry();
+var Chico = ee.FeatureCollection("ft:1mmRj4fN8mmvtynTxG56XMZJ-1y9n1i-lDUCIsXwV").geometry();
+
+// show the layers
+Map.addLayer(Paradise, {color: "280AC2"}, "Town of Paradise, California", 1, 1); //deep purple
+Map.addLayer(Magalia, {color: "91184E"}, "Town of Magalia, California", 1, 1); //reddish-purple
+Map.addLayer(Chico, {color: "1C06C2"}, "City of Chico, California", 1, 1); //deep blue
+
+//Center Map
+Map.centerObject(Paradise, 10);
+
+//L5SR Bands and Human-Friendly Naming
+var LANDSAT_7_BANDS = ["B1","B2","B3","B4","B5","B6","B7"];
+var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
+
+//Filtering against Paradise at one-year resolution.
+var landsat_SR = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR") //load LANDSAT7 raws
+	.filterBounds(Paradise)
+	.filterDate("2008-01-01","2009-01-01")
+	// Filter cloudy scenes.
+  .filter(ee.Filter.lt("CLOUD_COVER", 35))
+	.select(LANDSAT_7_BANDS, STD_NAMES);
+
+print(landsat_SR); //date debug
+
+var median = landsat_SR.median();
+
+//Display the Composite
+Map.addLayer(landsat_SR, {"bands":["red","green","blue"],min:0,max:2000}, "baselayer", 0, 1);
+
+function addNDVI(image) {
+  return image
+    .addBands(image.normalizedDifference(["nir","red"]).rename("ndvi"))
+  ;
+}
+
+var ndvi = addNDVI(median);
+
+Map.addLayer(ndvi,{bands:["ndvi"],min:0,max:1}, "ndvilayer", 0, 1);
+
+//predict bands
+var predictionBands = ["blue","green","red","nir","swir1","swir2","ndvi"];
+
+var trainingimage = ndvi.select(predictionBands);
+
+//fusion-table of polygons drawn in Google Earth Desktop
+var trainingpolygons = ee.FeatureCollection("ft:1DEQC9tLdjoERve9NKtLizNd19_eaoyk-Jslwogc-");
+
+var training = trainingimage.sampleRegions({
+    collection: trainingpolygons,
+    properties: ["class"],
+    scale: 30
+});
+
+//Train the CART classifier (a regular expression, not made up) with default parameters
+var trained = ee.Classifier.cart().train(training,"class", predictionBands);
+
+//Classify image with the same bands used for training.
+var CARTclassified = trainingimage.select(predictionBands).classify(trained);
+
+//Display the result using 0=barren, 1=urban, 2=green, 3=water
+Map.addLayer(CARTclassified, {min: 0, max: 3, palette: ["784800","FFF44F","228B22","97CAF9"]}, "CARTclassification", 1, 0.75);
+
+//Landsat True-Color Image Export
+Export.image.toDrive({
+  image: CARTclassified,
+  description: "classifiedImage_2008l7_Paradise_BigSquare",
+  folder: "California-Paradise_CampFire2018",
+  region:Big_Square,
+  scale:30.0
+});
