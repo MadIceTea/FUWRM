3d022118d53ca9ea13320733ed58b763f50ba4bb
chromebook <chromebook@madicetea.me>
2019-06-28 04:04:39 +0000

    Updated FireAnalysis-California/08-landcoverParadise_2019PostCampFire_landsat8.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/08-landcoverParadise_2019PostCampFire_landsat8.js
+++ FireAnalysis-California/08-landcoverParadise_2019PostCampFire_landsat8.js
@@ -412,7 +412,7 @@ Map.centerObject(Paradise, 10);
 var LANDSAT_8_BANDS = ["B2","B3","B4","B5","B6","B10","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
-//filtering landsat imagery in Paradise
+//Filtering against Paradise for what has past so far in 2019.
 var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 raws for during the fire period
 	.filterBounds(Paradise)
 	.filterDate("2019-01-01","2019-06-01")
