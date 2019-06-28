f0abf1dbd6665b7facc3922fe99957e75606bddc
chromebook <chromebook@madicetea.me>
2019-06-28 03:56:08 +0000

    Updated FireAnalysis-California/05b-landcoverParadise_2013landsat8.js (added 2 lines, deleted 1 line).

--- FireAnalysis-California/05b-landcoverParadise_2013landsat8.js
+++ FireAnalysis-California/05b-landcoverParadise_2013landsat8.js
@@ -24,7 +24,8 @@ Map.centerObject(Paradise, 10);
 var LANDSAT_8_BANDS = ["B2","B3","B4","B5","B6","B10","B7"];
 var STD_NAMES = ["blue","green","red","nir","swir1","tir","swir2"];
 
-//filtering Against Paradise a (near) 1-year resolution
+//Filtering against Paradise for almost one-year (2013).
+//Jan - early April 2013 data is not available yet for 2013.
 var landsat_SR = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") //load LANDSAT8 raws
 	.filterBounds(Paradise)
 	.filterDate("2013-01-01","2014-01-01")
