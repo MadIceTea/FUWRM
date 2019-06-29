87824ad8af7c55eff9c6452dd0b4df0f8f09d0ce
chromebook <chromebook@madicetea.me>
2019-06-29 16:41:54 +0000

    Updated FireAnalysis-California/bParadise-WindDir2018DuringFire.js (added 8 lines, deleted 1 line).

--- FireAnalysis-California/bParadise-WindDir2018DuringFire.js
+++ FireAnalysis-California/bParadise-WindDir2018DuringFire.js
@@ -1,3 +1,10 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
+        [[[-122.03757135752642, 40.231519880601745],
+          [-122.03757135752642, 39.49365087730002],
+          [-121.16965143565142, 39.49365087730002],
+          [-121.16965143565142, 40.231519880601745]]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 // Import the Province of Victoria from Fusion Table
 var Victoria = ee.FeatureCollection("ft:1UzSGq1cWUA5PloR9VxO7AVTu4vVXL3BHNiJKv6XB").geometry();
 
