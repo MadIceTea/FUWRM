af2fa451dbd1031e31b8ec348b88ba6bcdf69ce3
chromebook <chromebook@madicetea.me>
2019-06-29 06:09:01 +0000

    Updated FireAnalysis-Australia/34Victoria-KBDI2008-preCampFire.js (added 15 lines, deleted 1 line).

--- FireAnalysis-Australia/34Victoria-KBDI2008-preCampFire.js
+++ FireAnalysis-Australia/34Victoria-KBDI2008-preCampFire.js
@@ -1,3 +1,17 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var Big_Square = 
+    /* color: #ff6c64 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[144.84399398906248, -37.18091645226004],
+          [144.84399398906248, -38.13760786176182],
+          [146.16235336406248, -38.13760786176182],
+          [146.16235336406248, -37.18091645226004]]], null, false);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 // Import the Province of Victoria from Fusion Table
 var Victoria = ee.FeatureCollection("ft:1UzSGq1cWUA5PloR9VxO7AVTu4vVXL3BHNiJKv6XB").geometry();
 
