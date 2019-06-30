36cb090a7b1b070b395fe85c9ca1ac9c9089edd6
chromebook <chromebook@madicetea.me>
2019-06-30 06:12:11 +0000

    Deleted Datasets.

--- Datasets/Sample FIRMS.js
+++
@@ -1,9 +0,0 @@
-var dataset = ee.ImageCollection('FIRMS').filter(
-    ee.Filter.date('2018-07-21', '2019-08-31'));
-var fires = dataset.select('T21');
-var firesVis = {
-  min: 325.0,
-  max: 400.0,
-  palette: ['red', 'orange', 'yellow'],
-};
-Map.addLayer(fires, firesVis, 'Fires');
