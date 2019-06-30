c4c17ed27e4f54ca82ed7298b927b2a86dd9f161
chromebook <chromebook@madicetea.me>
2019-06-30 04:18:55 +0000

    Updated FireAnalysis-Australia/57Victoria-FireStationLocations.js (added 1 line, deleted 1 line).

--- FireAnalysis-Australia/57Victoria-FireStationLocations.js
+++ FireAnalysis-Australia/57Victoria-FireStationLocations.js
@@ -23,7 +23,7 @@ var FS =  ee.FeatureCollection("ft:1ipQ6dhXZGQbApdgJtgxYtFbc7fJH_ty2iy19TKfg").g
 var CFA =  ee.FeatureCollection("ft:1tS0GgWvzSa0_Nq_uBcZnC7irJJ45T-bybXBMBzXf").geometry();
 
 // show the layers
-Map.addLayer(Big_Square, {color: "55EAEC"}, "Region of Interest", 1, 0.35); //light blue
+Map.addLayer(Big_Square, {color: "55EAEC"}, "Region of Interest", 1, 0.15); //light blue
 Map.addLayer(FS, {color: "31994D"}, "Kinglake, VIC, Australia", 1, 1); //green
 Map.addLayer(CFA, {color: "C18AB9"}, "Drouin West, VIC, Australia", 1, 1); //purple
 Map.addLayer(firefight, {color: "ADC91F"}, "Tonimbuk, VIC, Australia", 0, 1); //darker green
