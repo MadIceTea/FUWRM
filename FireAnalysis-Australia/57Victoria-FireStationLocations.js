52c7660f54d00ee06350b56f9761ad30e2672e4f
chromebook <chromebook@madicetea.me>
2019-06-30 04:17:16 +0000

    Updated FireAnalysis-Australia/57Victoria-FireStationLocations.js (added 13 lines, deleted 25 lines).

--- FireAnalysis-Australia/57Victoria-FireStationLocations.js
+++ FireAnalysis-Australia/57Victoria-FireStationLocations.js
@@ -12,30 +12,18 @@ var Big_Square =
           [146.16235336406248, -38.13760786176182],
           [146.16235336406248, -37.18091645226004]]], null, false);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
-// Import the Province of Victoria from Fusion Table
-var Victoria = ee.FeatureCollection("ft:1UzSGq1cWUA5PloR9VxO7AVTu4vVXL3BHNiJKv6XB").geometry();
-
-// Also import the metropolitan regions of Melbourne, Buxton-Narbethlong-Marysville, and the Kinglake Cities
-var Melbourne = ee.FeatureCollection("ft:1IS6OpUOtWinTQd2KKzFHIyLQEOy_Mf-Pg7f7j1qz").geometry();
-var BuxNarbMary = ee.FeatureCollection("ft:1hseXyjCm5NM3krhX5qJB6-Gkl0e5o84tBD1WnbeJ").geometry();
-var Kinglake = ee.FeatureCollection("ft:1uTaTWzmTW02jGVsMa6AggqK6ncqY2527kJQh1t1z").geometry();
-
-//Also import the small town regions of Labertouche, DrouinWest, and Tonimbuk which burned in the Bunyip State Park Fire
-var Labertouche = ee.FeatureCollection("ft:1Wsbt3y1em75OyN2vfvQhHjbI7XTptMFQFeT069n5").geometry();
-var DrouinWest = ee.FeatureCollection("ft:1g2cJ9z_o3gnFKfYxbPtF6DYgBOe-JUDoNHRhJqBQ").geometry();
-var Tonimbuk = ee.FeatureCollection("ft:1tqsM6Mt1HQD5QE_VF3vyDcj7beSlX0YQzHDSa2dd").geometry();
-
-// show the layers
-Map.addLayer(Victoria, {color: "55EAEC"}, "Province of Victoria, Australia", 1, 0.4); //light blue
-Map.addLayer(Melbourne, {color: "000000"}, "Melbourne, VIC, Australia", 1, 0.3); //black
-Map.addLayer(BuxNarbMary, {color: "BF19DB"}, "Buxton-Narbelthong-Marysville, VIC, Australia", 1, 1); //purple
-Map.addLayer(Kinglake, {color: "31994D"}, "Kinglake, VIC, Australia", 1, 1); //green
-Map.addLayer(Labertouche, {color: "270BFF"}, "Labertouche, VIC, Australia", 1, 1); //royal-deep blue
-Map.addLayer(DrouinWest, {color: "C18AB9"}, "Drouin West, VIC, Australia", 1, 1); //purple
-Map.addLayer(Tonimbuk, {color: "ADC91F"}, "Tonimbuk, VIC, Australia", 1, 1); //darker green
-
 //Center Map
 Map.centerObject(Big_Square, 9);
 
-//The list of fire stations
-var collection = ee.FeatureCollection("ft:TODO").geometry();
+//The longer list of all firefighting locations
+var firefight = ee.FeatureCollection("ft:1b9SQEYUNBQCghsQCkUlAfarb8feC4dNe87l9eaaj").geometry();
+
+//The broken-down list of formal Fire Stations (FS) and Country Fire Authorities (CFA)
+var FS =  ee.FeatureCollection("ft:1ipQ6dhXZGQbApdgJtgxYtFbc7fJH_ty2iy19TKfg").geometry();
+var CFA =  ee.FeatureCollection("ft:1tS0GgWvzSa0_Nq_uBcZnC7irJJ45T-bybXBMBzXf").geometry();
+
+// show the layers
+Map.addLayer(Big_Square, {color: "55EAEC"}, "Region of Interest", 1, 0.2); //light blue
+Map.addLayer(FS, {color: "31994D"}, "Kinglake, VIC, Australia", 1, 0.85); //green
+Map.addLayer(CFA, {color: "C18AB9"}, "Drouin West, VIC, Australia", 1, 75); //purple
+Map.addLayer(firefight, {color: "ADC91F"}, "Tonimbuk, VIC, Australia", 0, 1); //darker green
