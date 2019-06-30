e0ab676cd07b68306df06933bb9c553999767693
chromebook <chromebook@madicetea.me>
2019-06-30 07:39:57 +0000

    Updated FireAnalysis-California/40LIDA_Paradise_MOLUSCE50kSim_2040.js (added 3 lines, deleted 3 lines).

--- FireAnalysis-California/40LIDA_Paradise_MOLUSCE50kSim_2040.js
+++ FireAnalysis-California/40LIDA_Paradise_MOLUSCE50kSim_2040.js
@@ -5,7 +5,7 @@ var Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
           [-121.16965143565142, 39.49365087730002],
           [-121.16965143565142, 40.231519880601745]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
-var CARTclassified = ee.Image("TODO:2040 image");
+var CARTclassified = ee.Image("users/GEE_Alex/MOLUSCEsims_USA/LIDAParadise_2040_Simulation");
 
 // Import the primary region of interest, Town of Paradise which burned in the Camp Fire of 2018.
 var Paradise = ee.FeatureCollection("ft:1JIO1SLcMe08lHJWUIP7zWpW5razN6FfIwibHtcje").geometry();
@@ -50,7 +50,7 @@ single = single.addBands(mask);
 //Landsat True-Color Image Export
 Export.image.toDrive({
   image: single,
-  description: "classifiedImage_2018_MOLUSCEsim50k_Paradise_BigSquare",
+  description: "classifiedImage_2040_MOLUSCEsim50k_Paradise_BigSquare",
   folder: "California-Paradise_CampFire2018",
   region:Big_Square,
   scale:30.0,
@@ -61,7 +61,7 @@ Export.image.toDrive({
 
 Export.image.toAsset({
   image: single,
-  description: "classifiedImage_2018_MOLUSCEsim50k_Paradise_BigSquare-Asset",
+  description: "classifiedImage_2040_MOLUSCEsim50k_Paradise_BigSquare-Asset",
   region:Big_Square,
   scale:30.0,
   crs: "EPSG:3857"
