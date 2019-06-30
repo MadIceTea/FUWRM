d967dcf9b9ac97c474aa3663ef494de2da98afe6
chromebook <chromebook@madicetea.me>
2019-06-30 01:03:12 +0000

    Updated FireAnalysis-Australia/56Victoria-GSMaPRain2019.js (added 3 lines, deleted 2 lines).

--- FireAnalysis-Australia/56Victoria-GSMaPRain2019.js
+++ FireAnalysis-Australia/56Victoria-GSMaPRain2019.js
@@ -37,9 +37,10 @@ Map.addLayer(Tonimbuk, {color: "ADC91F"}, "Tonimbuk, VIC, Australia", 1, 1); //d
 //Center Map
 Map.centerObject(Big_Square, 9);
 
-var collection = ee.ImageCollection("JAXA/GPM_L3/GSMaP/v6/reanalysis")
+var collection = ee.ImageCollection("JAXA/GPM_L3/GSMaP/v6/operational")
   .select("hourlyPrecipRateGC")
-  .filterDate("2019-01-01", "2019-05-01");
+  .filterDate("2019-01-01", "2019-05-01")
+  .filterBounds(Melbourne);
 
 var single = collection.mean();
 
