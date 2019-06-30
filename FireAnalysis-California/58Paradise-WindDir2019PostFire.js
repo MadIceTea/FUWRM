bae858433422002accbd06bcdc57d35099994251
chromebook <chromebook@madicetea.me>
2019-06-30 02:28:09 +0000

    Updated FireAnalysis-California/58Paradise-WindDir2019PostFire.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/58Paradise-WindDir2019PostFire.js
+++ FireAnalysis-California/58Paradise-WindDir2019PostFire.js
@@ -33,7 +33,7 @@ var band_viz = {
 
 var single = collection.mean();
 
-Map.addLayer(single, band_viz, "Yearly Average Wind Directionality", 1, 0.85);
+Map.addLayer(single, band_viz, "Yearly Average Wind Direction", 1, 0.85);
 
 //True-Color Image Export
 //Export Image
