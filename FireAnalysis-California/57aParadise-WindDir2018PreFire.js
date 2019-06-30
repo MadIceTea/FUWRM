4cf28ca70bfd66c3c7e41f1cb56a2815818b3f59
chromebook <chromebook@madicetea.me>
2019-06-30 02:53:19 +0000

    Updated FireAnalysis-California/57aParadise-WindDir2018PreFire.js (added 1 line, deleted 1 line).

--- FireAnalysis-California/57aParadise-WindDir2018PreFire.js
+++ FireAnalysis-California/57aParadise-WindDir2018PreFire.js
@@ -66,7 +66,7 @@ single = single.addBands(mask);
 Export.image.toDrive({
   image: single,
   description: "WindDirColored_preFire2018_Paradise_BigSquare",
-  folder: "Australia-Victoria_BlackFire2009",
+  folder: "California-Paradise_CampFire2018",
   region:Big_Square,
   scale:30.0,
   fileFormat: "GeoTIFF",
