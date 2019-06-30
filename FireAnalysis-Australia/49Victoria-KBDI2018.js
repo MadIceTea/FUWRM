c90df26772c10d367d38c11a5c4256edc6e3d345
chromebook <chromebook@madicetea.me>
2019-06-30 00:58:34 +0000

    Updated FireAnalysis-Australia/49Victoria-KBDI2018.js (added 2 lines, deleted 1 line).

--- FireAnalysis-Australia/49Victoria-KBDI2018.js
+++ FireAnalysis-Australia/49Victoria-KBDI2018.js
@@ -39,7 +39,8 @@ Map.centerObject(Big_Square, 9);
 
 var collection = ee.ImageCollection('UTOKYO/WTLAB/KBDI/v1')
   .select("KBDI")
-  .filterDate("2018-01-01", "2019-01-01");
+  .filterDate("2018-01-01", "2019-01-01")
+  .filterBounds(Melbourne);
   
 var band_viz = {
   min: 0,
