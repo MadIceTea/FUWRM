8e2048c42c0588df5114dd95bd2c9d1a10e5f31f
chromebook <chromebook@madicetea.me>
2019-06-30 00:48:56 +0000

    Updated FireAnalysis-Australia/35Victoria-GLDASTemp2008.js (added 2 lines, deleted 1 line).

--- FireAnalysis-Australia/35Victoria-GLDASTemp2008.js
+++ FireAnalysis-Australia/35Victoria-GLDASTemp2008.js
@@ -39,7 +39,8 @@ Map.centerObject(Big_Square, 9);
 
 var collection = ee.ImageCollection("NASA/GLDAS/V021/NOAH/G025/T3H")
   .select("SoilTMP0_10cm_inst")
-  .filterDate("2008-01-01", "2009-01-01");
+  .filterDate("2008-01-01", "2009-01-01")
+  .filterBounds(Melbourne);
 
 var band_viz = {
   min: 273.15, //0C
