9d657f8dc1449a89e960aadc2d38c9a05ab707c5
chromebook <chromebook@madicetea.me>
2019-06-30 04:03:10 +0000

    Created FireAnalysis-California/65Paradise-FireStationLocations.js.

---
+++ FireAnalysis-California/65Paradise-FireStationLocations.js
@@ -0,0 +1,23 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var Big_Square = /* color: #acc235 */ee.Geometry.Polygon(
+        [[[-122.03757135752642, 40.231519880601745],
+          [-122.03757135752642, 39.49365087730002],
+          [-121.16965143565142, 39.49365087730002],
+          [-121.16965143565142, 40.231519880601745]]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// Import the primary region of interest, Town of Paradise which burned in the Camp Fire of 2018.
+var Paradise = ee.FeatureCollection("ft:1JIO1SLcMe08lHJWUIP7zWpW5razN6FfIwibHtcje").geometry();
+
+//Also import the small town region of Magalia to the north, and the larger city of Chico to the west.
+var Magalia = ee.FeatureCollection("ft:1BCMRnYS4plV2NVWtP6dZHYmE1V00kY8baAPU9Udm").geometry();
+var Chico = ee.FeatureCollection("ft:1mmRj4fN8mmvtynTxG56XMZJ-1y9n1i-lDUCIsXwV").geometry();
+
+// show the layers
+Map.addLayer(Paradise, {color: "280AC2"}, "Town of Paradise, California", 1, 1); //deep purple
+Map.addLayer(Magalia, {color: "91184E"}, "Town of Magalia, California", 1, 1); //reddish-purple
+Map.addLayer(Chico, {color: "1C06C2"}, "City of Chico, California", 1, 1); //deep blue
+
+//Center Map
+Map.centerObject(Paradise, 10);
+
+var collection = 
